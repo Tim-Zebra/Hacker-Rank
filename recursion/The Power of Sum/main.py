211113414de3803753765c38610a1a6a046671df
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-10-04 12:23:13 -0500

    added base code with psuedocode

---
+++ recursion/The Power of Sum/main.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'powerSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER X
+#  2. INTEGER N
+#
+
+def powerSum(X, N):
+    # Write your code here
+    
+    # if statement that returns at the start if cannot be reduced
+    # Take the root of the number, this will be the max value attainable
+    # return posCombosNum;
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    X = int(input().strip())
+
+    N = int(input().strip())
+
+    result = powerSum(X, N)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
