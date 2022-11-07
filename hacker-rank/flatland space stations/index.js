c5502b94f9d2c1d9d916523d098cfd5a4e10a7c0
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-07 12:08:47 -0600

    built base code

--- hacker-rank/flatland space stations/index.js
+++ hacker-rank/flatland space stations/index.js
@@ -1,5 +1,23 @@
 // Complete the flatlandSpaceStations function below.
+// n = number of cities
+// c = an array of cities with space stations
+
+// eg. n = 3 means there are 3 cities. c[1] means that city 1 has a space station. The cities are 0, 1, 2.
+// distance = 1km which is the subtractive value between cities difference between city 1 and 2 is 1, 0 and 1 is 1, and 2 and 0 is 2.
+
+// return maxiumum distance
 function flatlandSpaceStations(n, c) {
+  let maxDistance = 0;
+  let numberOfCities = n;
+  let citiesWithStations = c;
+  let distanceOfCityFromStation = [];
 
+  for(let k = 0; k < n; k++) {
+    distanceOfCityFromStation.push(0);
+  }
 
+  // find the difference of each city to the nearest space station;
+
+  console.log('thisHappened', maxDistance);
+  return maxDistance;
 }
