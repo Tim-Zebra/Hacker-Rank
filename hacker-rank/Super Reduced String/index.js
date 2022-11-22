338cd054544b573541e08e3496201d7b3a89cfe6
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-21 20:18:20 -0600

    added logic for reducing the array. Creating a new array with acceptave criteria

--- hacker-rank/Super Reduced String/index.js
+++ hacker-rank/Super Reduced String/index.js
@@ -2,13 +2,30 @@ function superReducedString(s) {
   let newString = "Empty String";
   // Checks argument string
   if(s.length > 0) {
-    newString = s.split();
+    newString = s.split('');
   } else {
     return newString;
   }
 
+  console.log('thisHappened', newString);
   // Runs string reducing function
+  let updatedString = '';
+  for(let k = 0; k < newString.length-1; k++) {
+    let index0 = newString[k];
+    let index1 = newString[k+1];
+    if (index0 !== index1) {
+      updatedString += index0;
+    }
+  }
+
+  if(updatedString === '') {
+    return "Empty String"
+  }
+  console.log('thisHappened', updatedString);
+  return newString;
 
 }
 
+
 // Tests
+superReducedString('abba');
