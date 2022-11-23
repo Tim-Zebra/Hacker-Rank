71ba41c4fcbee8fa76d44c6ce79cb8d6d509cfec
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-22 19:01:54 -0600

    added pseudocode

--- hacker-rank/Tower Breakers/index.js
+++ hacker-rank/Tower Breakers/index.js
@@ -1,8 +1,16 @@
 // n is an Integer denoting the NUMBER of TOWERS
 // m is an Interger denoting the HEIGHT of EACH tower
+
+// 2 Players. Player 1 always goes first
 function towerBreakers(n, m) {
   // Write your code here
 
+  // Player 1 Turn
+  // Player 2 Turn
+  // Repeat until game ends
+
+  // Return 1 if Player 1 wins, or 2 if Player 2 wins
 }
 
 //tests
+towerBreakers()
