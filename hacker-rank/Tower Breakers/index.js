f4859a67383bd74eed718722b94bc16939df96a8
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-22 21:32:19 -0600

    need to write out algo and find pattern. Examples hint at only a set amount of options, but following the logic, there should be at least one more option' git push

--- hacker-rank/Tower Breakers/index.js
+++ hacker-rank/Tower Breakers/index.js
@@ -1,7 +1,8 @@
 // n is an Integer denoting the NUMBER of TOWERS
 // m is an Interger denoting the HEIGHT of EACH tower
 
-// Players can reduce their tower height by y, which must evenly divide into tower height. Cannot divide by 1
+// Players can reduce their tower height of x by y. And y must evenly divide into x.
+// Remove a number greater than 1 but less than x. Ok to use 1 if your tower height is 2
 // When the tower is evenly divided by height: newTowerHeight = towerHeight - y;
 
 // 2 Players. Player 1 always goes first
