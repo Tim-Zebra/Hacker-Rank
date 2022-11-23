464b122a27e95ae00a1a4d0830b1bc3c1f37b914
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-22 19:04:59 -0600

    added base of function

--- hacker-rank/Tower Breakers/index.js
+++ hacker-rank/Tower Breakers/index.js
@@ -1,13 +1,17 @@
 // n is an Integer denoting the NUMBER of TOWERS
 // m is an Interger denoting the HEIGHT of EACH tower
 
+// Players can reduce their tower height by y, which must evenly divide into tower height. Cannot divide by 1
+// When the tower is evenly divided by height: newTowerHeight = towerHeight - y;
+
 // 2 Players. Player 1 always goes first
+
 function towerBreakers(n, m) {
   // Write your code here
 
   // Player 1 Turn
   // Player 2 Turn
-  // Repeat until game ends
+  // Repeat turns until game ends
 
   // Return 1 if Player 1 wins, or 2 if Player 2 wins
 }
