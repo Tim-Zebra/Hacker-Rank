09fbc79d3ee4fb12e8b63ace79b693c2ec89a744
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-11-28 17:38:35 -0600

    added pseudocode. Seems like part 2 is the same as part 1, except info is given as an array.

--- hacker-rank/Tower Breakers, PART 2/index.js
+++ hacker-rank/Tower Breakers, PART 2/index.js
@@ -1,4 +1,18 @@
 function towerBreakers(arr) {
   // Write your code here
+  let maxTowerHeight = Math.max(arr);
+  let arrayLength = arr.length;
+    if(maxTowerHeight === 1) {
+      return 2;
+    }
+    if(arrayLength % 2 === 0) {
+      return 1;
+    } else {
+      return 2;
+    }
+    // Player 1 Turn
+    // Player 2 Turn
+    // Repeat turns until game ends
+  
 
 }
